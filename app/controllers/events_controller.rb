db81f29ecfd6213f1cbe4729a847d8c6b14e42ce
Amelia Davis <ameliadavis271@gmail.com>
2021-02-03 22:48:42 -0800

    Events CRUD finalised!

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -9,7 +9,7 @@ before_action :set_event, only: [:show, :destroy, :update]
 
     def show
         @event = Event.find(params[:id])
-        render json: event
+        render json: @event
     end
 
     def create
@@ -24,6 +24,11 @@ before_action :set_event, only: [:show, :destroy, :update]
     end
 
     def update
+        if @event.update(update_events_params)
+            render status: :created
+        else
+            render status: :bad_request
+        end
     end
 
     def destroy
@@ -41,6 +46,10 @@ before_action :set_event, only: [:show, :destroy, :update]
     # strong params
     def events_params
         params.require(:event).permit(:name, :description, :date, :time)
+    end
+
+    def update_events_params
+        params.require(:event).permit(:name, :description, :time)
     end
 
 end
