4263ea425da9b0cda9e44d8ea42a301a4506f9d5
Ben Ahale <ahaleben@gmail.com>
2021-02-05 18:51:08 +1100

    Database changes for families and users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_02_020834) do
+ActiveRecord::Schema.define(version: 2021_02_04_073437) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,16 +22,26 @@ ActiveRecord::Schema.define(version: 2021_02_02_020834) do
     t.string "time"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_events_on_user_id"
+  end
+
+  create_table "families", force: :cascade do |t|
+    t.string "name"
+    t.string "family_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
-    t.integer "family_id"
     t.string "email"
     t.string "first_name"
     t.string "last_name"
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "family_id"
   end
 
+  add_foreign_key "events", "users"
 end
