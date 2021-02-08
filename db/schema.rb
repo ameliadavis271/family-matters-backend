8b33b3f183456d2c415907a0d9937610adbd4cdb
Ben Ahale <ahaleben@gmail.com>
2021-02-08 19:16:02 +1100

    Created memories model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_04_073437) do
+ActiveRecord::Schema.define(version: 2021_02_08_011601) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "albums", force: :cascade do |t|
+    t.string "name"
+    t.bigint "family_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["family_id"], name: "index_albums_on_family_id"
+  end
 
   create_table "events", force: :cascade do |t|
     t.string "name"
@@ -22,8 +30,8 @@ ActiveRecord::Schema.define(version: 2021_02_04_073437) do
     t.string "time"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.bigint "user_id", null: false
-    t.index ["user_id"], name: "index_events_on_user_id"
+    t.bigint "family_id", null: false
+    t.index ["family_id"], name: "index_events_on_family_id"
   end
 
   create_table "families", force: :cascade do |t|
@@ -31,6 +39,15 @@ ActiveRecord::Schema.define(version: 2021_02_04_073437) do
     t.string "family_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "memories", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.bigint "family_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["family_id"], name: "index_memories_on_family_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -43,5 +60,7 @@ ActiveRecord::Schema.define(version: 2021_02_04_073437) do
     t.string "family_id"
   end
 
-  add_foreign_key "events", "users"
+  add_foreign_key "albums", "families"
+  add_foreign_key "events", "families"
+  add_foreign_key "memories", "families"
 end
