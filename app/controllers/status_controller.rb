928739600c8661854d3c112ae7a9cd58910faa11
Ben Ahale <ahaleben@gmail.com>
2021-02-09 18:52:46 +1100

    Fix for family ID

--- app/controllers/status_controller.rb
+++ app/controllers/status_controller.rb
@@ -2,15 +2,11 @@ class StatusController < ApplicationController
   before_action :authenticate_user
 
   def index
-    render json: { message: "Logged In"}
+    @family = current_user.family
+    render json: { familyid: @family.family_id }
   end
 
   def user
     render json: { user: current_user.email }
-  end
-
-  def family_id
-    @family = current_user.family
-    render json: { familyid: @family.family_id }
   end
 end
