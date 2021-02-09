e3080a42f07ad5c0039489c754a9b278142bb52f
Ben Ahale <ahaleben@gmail.com>
2021-02-09 18:28:01 +1100

    Added localhost to CORS origins

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'family-matters.netlify.app', 'family-matters-app.netlify.app'
+    origins 'family-matters.netlify.app', 'family-matters-app.netlify.app', 'localhost:8080'
 
     resource '*',
       headers: :any,
